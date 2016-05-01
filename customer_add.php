5868e90642b46a29164c23088304667509135bfa
dqiao <deeqiiao@gmail.com>
2016-04-30 23:50:35 -0500

    editted the format of customer_add.html with bootstrap navbar

--- customer_add.php
+++ customer_add.php
@@ -1,3 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adding Customer To DB</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<!--Bootstrap Navbar-->	
+	<nav class="navbar navbar-default">
+	<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		<a class="navbar-brand" href="#">Assignment 2</a>
+		</div>
+
+		<!-- Nav Bar Links-->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<!--Need to change to customer_view.html but find a way to link the customer_view.php to customer_view.html-->
+				<li><a href="customer_view.php">View</a></li>
+				<li class="active"><a href="#">Add<span class="sr-only">(current)</span></a></li>
+			</ul>
+		</div> <!--End of 2 links-->
+	</nav> <!--End of nav bar-->
+
 <?php
 //testing 
 //newTesting
@@ -34,3 +69,5 @@
 	
 ?>
 
+</body>
+</html>
